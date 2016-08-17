7313e06866903a979bf40fa9c9114351687b855d
xukai <xukai@wsncm.com>
2016-08-17 22:42:36 +0800

    增加UDP示例

---
+++ review_demo/udp_echo_demo/udp_client.py
@@ -0,0 +1,36 @@
+import asyncio
+
+class EchoClientProtocol:
+    def __init__(self, message, loop):
+        self.message = message
+        self.loop = loop
+        self.transport = None
+
+    def connection_made(self, transport):
+        self.transport = transport
+        print('Send:', self.message)
+        self.transport.sendto(self.message.encode())
+
+    def datagram_received(self, data, addr):
+        print("Received:", data.decode())
+
+        print("Close the socket")
+        self.transport.close()
+
+    def error_received(self, exc):
+        print('Error received:', exc)
+
+    def connection_lost(self, exc):
+        print("Socket closed, stop the event loop")
+        loop = asyncio.get_event_loop()
+        loop.stop()
+
+loop = asyncio.get_event_loop()
+message = "Hello World!"
+connect = loop.create_datagram_endpoint(
+    lambda: EchoClientProtocol(message, loop),
+    remote_addr=('127.0.0.1', 9999))
+transport, protocol = loop.run_until_complete(connect)
+loop.run_forever()
+transport.close()
+loop.close()
