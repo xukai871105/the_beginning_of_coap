83f751835f4b30f4d46b48bf3a6a9d5feaeeb4bd
xukai <xukai@wsncm.com>
2016-08-20 21:11:19 +0800

    增加http_demo

---
+++ review_demo/http_demo/app.py
@@ -0,0 +1,17 @@
+﻿# -*- coding: utf-8 -*-
+from flask import Flask, jsonify, render_template, request
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+# 主页面
+    return render_template('index.html')
+    
+@app.route('/add')
+def add():
+    a = request.args.get('a', 0, type=int)
+    b = request.args.get('b', 0, type=int)
+    return jsonify(result = a + b)
+    
+if __name__=="__main__":
+    app.run(host = '0.0.0.0',port = 8080, debug = True)
