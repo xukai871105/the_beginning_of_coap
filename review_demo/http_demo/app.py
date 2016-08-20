0691c2645f3968325e571149f8af8fc387590714
xukai <xukai@wsncm.com>
2016-08-21 04:21:07 +0800

    更新HTTP示例

--- review_demo/http_demo/app.py
+++ review_demo/http_demo/app.py
@@ -7,10 +7,15 @@ def index():
 # 主页面
     return render_template('index.html')
     
-@app.route('/add')
-def add():
+@app.route('/value', methods=['GET', 'POST'])
+def cal_value():
+  if request.method == 'GET':
     a = request.args.get('a', 0, type=int)
     b = request.args.get('b', 0, type=int)
+    return jsonify(result = a + b)
+  elif request.method == 'POST':
+    a = request.form.get('a', 0, type=int)
+    b = request.form.get('b', 0, type=int)
     return jsonify(result = a + b)
     
 if __name__=="__main__":
