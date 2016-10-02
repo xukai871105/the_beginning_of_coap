cb44299b1c61b1600592366d9fe4e9e23d274fb7
xukai <xukai@wsncm.com>
2016-10-02 16:13:41 +0800

    增加获取设备列表函数

--- microsystem_server/routes/index.js
+++ microsystem_server/routes/index.js
@@ -30,44 +30,21 @@ router.get('/test', function (req, res) {
 
 router.get('/', function (req, res) {
 
-	var conn = mysql.createConnection(config);
-	conn.connect();
-
-	conn.query("SELECT DISTINCT device_id FROM sensor_history",
-		function(err, rows) {
-		if (!err) {
-			var device_id = rows[0].device_id
-			var url = '/devices/' + device_id + '/data.json';
-			res.render('admin.html', {url: url, device_id: device_id, devices: rows});
-		}
-	});
-	
-	conn.end(function(err) {
-		if (!err) {
-			console.log("db close");
-		}
-	});	
+	get_device_list(function(rows){
+		var device_id = rows[0].device_id
+		var url = '/devices/' + device_id + '/data.json';
+		res.render('admin.html', {url: url, device_id: device_id, devices: rows});		
+	})
 });
 
 router.get('/devices/:device_id/page', function (req, res) {
 
 	var device_id = req.params.device_id;
 	var url = '/devices/' + device_id +'/data.json';
-	var conn = mysql.createConnection(config);
-	conn.connect();
 
-	conn.query("SELECT DISTINCT device_id FROM sensor_history",
-		function(err, rows) {
-		if (!err) {
-			res.render('admin.html', {url: url, device_id: device_id, devices: rows});
-		}
-	});
-	
-	conn.end(function(err){
-		if (!err) {
-			console.log("db close");
-		}
-	});	
+	get_device_list(function(rows){
+		res.render('admin.html', {url: url, device_id: device_id, devices: rows});	
+	})	
 });
 
 router.get('/devices/:device_id/data.json', function (req, res) {
@@ -88,7 +65,7 @@ router.get('/devices/:device_id/data.json', function (req, res) {
 		console.log('connected as id ' + conn.threadId);
 	});
 	
-	conn.query("SELECT COUNT(*) as total FROM sensor_history WHERE device_id=?", 
+	conn.query('SELECT COUNT(*) as total FROM sensor_history WHERE device_id=?', 
 		[device_id],
 		function(err, result) {
 			if (!err) {
@@ -97,7 +74,7 @@ router.get('/devices/:device_id/data.json', function (req, res) {
 			}
 	});
 
-	conn.query('SELECT * FROM sensor_history WHERE device_id=? order by id desc limit ?, ? ', 
+	conn.query('SELECT * FROM sensor_history WHERE device_id=? order by id desc limit ?, ?', 
 		[device_id, offset, limit],
 		function(err, result) {
 			if (!err) {
@@ -115,4 +92,17 @@ router.get('/devices/:device_id/data.json', function (req, res) {
 	});
 });
 
+function get_device_list(callback) {
+	var conn = mysql.createConnection(config);
+	conn.connect();
+
+	conn.query("SELECT DISTINCT device_id FROM sensor_history",
+		function(err, rows) {
+		if (!err) {
+			callback(rows);
+		}
+	});
+	
+	conn.end();	
+}
 module.exports = router;
