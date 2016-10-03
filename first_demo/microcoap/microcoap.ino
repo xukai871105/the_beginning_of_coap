e9833d5c109c47e60ead182b43245e9fe40d262e
xukai <xukai@wsncm.com>
2016-10-03 22:41:05 +0800

    修改入门示例代码格式

--- first_demo/microcoap/microcoap.ino
+++ first_demo/microcoap/microcoap.ino
@@ -61,38 +61,38 @@ static int handle_put_light(coap_rw_buffer_t *scratch,
                             const coap_packet_t *inpkt, coap_packet_t *outpkt, 
                             uint8_t id_hi, uint8_t id_lo)
 {
-    if (inpkt->payload.len == 0)
-        return coap_make_response(scratch, outpkt, 
-                                    NULL, 0, 
-                                    id_hi, id_lo, &inpkt->tok, 
-                                    COAP_RSPCODE_BAD_REQUEST, 
-                                    COAP_CONTENTTYPE_TEXT_PLAIN);
-    if (inpkt->payload.p[0] == '1')
-    {
-        light = '1';
-        digitalWrite(led, HIGH);
-        Serial.println("ON");
-    }
-    else
-    {
-        light = '0';
-        digitalWrite(led, LOW);
-        Serial.println("OFF");
-    }
+  if (inpkt->payload.len == 0)
     return coap_make_response(scratch, outpkt, 
-                                (const uint8_t *)&light, 1, 
-                                id_hi, id_lo, &inpkt->tok, 
-                                COAP_RSPCODE_CHANGED, 
-                                COAP_CONTENTTYPE_TEXT_PLAIN);
+                        NULL, 0, 
+                        id_hi, id_lo, &inpkt->tok, 
+                        COAP_RSPCODE_BAD_REQUEST, 
+                        COAP_CONTENTTYPE_TEXT_PLAIN);
+  if (inpkt->payload.p[0] == '1')
+  {
+      light = '1';
+      digitalWrite(led, HIGH);
+      Serial.println("ON");
+  }
+  else
+  {
+      light = '0';
+      digitalWrite(led, LOW);
+      Serial.println("OFF");
+  }
+  return coap_make_response(scratch, outpkt, 
+                              (const uint8_t *)&light, 1, 
+                              id_hi, id_lo, &inpkt->tok, 
+                              COAP_RSPCODE_CHANGED, 
+                              COAP_CONTENTTYPE_TEXT_PLAIN);
 }
 
 coap_endpoint_t endpoints[] =
 {
-    {COAP_METHOD_GET, handle_get_well_known_core, &path_well_known_core, "ct=40"},
-    {COAP_METHOD_GET, handle_get_hello, &path_hello, "ct=0"},
-    {COAP_METHOD_GET, handle_get_light, &path_light, "ct=0"},
-    {COAP_METHOD_PUT, handle_put_light, &path_light, NULL},
-    {(coap_method_t)0, NULL, NULL, NULL}
+  {COAP_METHOD_GET, handle_get_well_known_core, &path_well_known_core, "ct=40"},
+  {COAP_METHOD_GET, handle_get_hello, &path_hello, "ct=0"},
+  {COAP_METHOD_GET, handle_get_light, &path_light, "ct=0"},
+  {COAP_METHOD_PUT, handle_put_light, &path_light, NULL},
+  {(coap_method_t)0, NULL, NULL, NULL}
 };
 
 void build_rsp(const coap_endpoint_t *ep)
