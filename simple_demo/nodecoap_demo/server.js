f79b26b61702838c52ddff6c975a729cd174674b
xukai <xukai@wsncm.com>
2016-10-17 22:57:42 +0800

    增加调试内容

--- simple_demo/nodecoap_demo/server.js
+++ simple_demo/nodecoap_demo/server.js
@@ -2,9 +2,9 @@ const coap    = require('coap')
 const server  = coap.createServer()
 
 server.on('request', function(req, res) {
-  console.log(req.url)
-  console.log(req.method)
+  console.log('method:' + req.method + ' url:' + req.url)
   if (req.method == 'GET' && req.url.split('/')[1] == 'time') {
+    console.log(new Date().toLocaleString())
     res.end(new Date().toLocaleString())
   }
 })
