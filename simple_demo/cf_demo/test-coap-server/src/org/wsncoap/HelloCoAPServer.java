e46c8b8f36c0e851248a15b1144cf46c3881f429
xukai <xukai@wsncm.com>
2016-12-18 19:57:59 +0800

    重新增加cf_demo

---
+++ simple_demo/cf_demo/test-coap-server/src/org/wsncoap/HelloCoAPServer.java
@@ -0,0 +1,60 @@
+package org.wsncoap;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.SocketException;
+
+import org.eclipse.californium.core.CoapResource;
+import org.eclipse.californium.core.CoapServer;
+import org.eclipse.californium.core.network.CoapEndpoint;
+import org.eclipse.californium.core.network.EndpointManager;
+import org.eclipse.californium.core.network.config.NetworkConfig;
+import org.eclipse.californium.core.server.resources.CoapExchange;
+
+public class HelloCoAPServer extends CoapServer {
+	private static final int COAP_PORT = NetworkConfig.getStandard().getInt(NetworkConfig.Keys.COAP_PORT);
+    /*
+     * Application entry point.
+     */
+    public static void main(String[] args) {
+        
+        try {
+
+        	HelloCoAPServer server = new HelloCoAPServer();
+ 
+            server.addEndpoints();
+            server.start();
+
+        } catch (SocketException e) {
+            System.err.println("Failed to initialize server: " + e.getMessage());
+        }
+    }
+
+    private void addEndpoints() {
+    	for (InetAddress addr : EndpointManager.getEndpointManager().getNetworkInterfaces()) {
+    		// only binds to IPv4 addresses and localhost
+			if (addr instanceof Inet4Address || addr.isLoopbackAddress()) {
+				InetSocketAddress bindToAddress = new InetSocketAddress(addr, COAP_PORT);
+				addEndpoint(new CoapEndpoint(bindToAddress));
+			}
+		}
+    }
+
+    public HelloCoAPServer() throws SocketException {
+        add(new HelloWorldResource());
+    }
+
+    class HelloWorldResource extends CoapResource {
+        
+        public HelloWorldResource() {
+            super("hello");
+            getAttributes().setTitle("Hello-World Resource");
+        }
+
+        @Override
+        public void handleGET(CoapExchange exchange) {
+            exchange.respond("Hello CoAP!");
+        }
+    }
+}
