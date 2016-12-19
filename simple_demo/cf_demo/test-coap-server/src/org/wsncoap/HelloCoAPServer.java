744b860bf177aea431deb3ef4dfd8a6bde8837ce
xukai <xukai@wsncm.com>
2016-12-19 13:11:04 +0800

    更新CoAP服务器示例

--- simple_demo/cf_demo/test-coap-server/src/org/wsncoap/HelloCoAPServer.java
+++ simple_demo/cf_demo/test-coap-server/src/org/wsncoap/HelloCoAPServer.java
@@ -1,59 +1,34 @@
 package org.wsncoap;
 
-import java.net.Inet4Address;
-import java.net.InetAddress;
-import java.net.InetSocketAddress;
-import java.net.SocketException;
+import java.text.SimpleDateFormat; 
 import java.util.Date;
 
 import org.eclipse.californium.core.CoapResource;
 import org.eclipse.californium.core.CoapServer;
 import org.eclipse.californium.core.coap.CoAP.ResponseCode;
-import org.eclipse.californium.core.network.CoapEndpoint;
-import org.eclipse.californium.core.network.EndpointManager;
-import org.eclipse.californium.core.network.config.NetworkConfig;
 import org.eclipse.californium.core.server.resources.CoapExchange;
 
-public class HelloCoAPServer extends CoapServer {
-	private static final int COAP_PORT = NetworkConfig.getStandard().getInt(NetworkConfig.Keys.COAP_PORT);
-    /*
-     * Application entry point.
-     */
+public class HelloCoAPServer {
+
     public static void main(String[] args) {
         
-        try {
-        	HelloCoAPServer server = new HelloCoAPServer();
-            // server.addEndpoints();
-            server.start();
-        } catch (SocketException e) {
-            System.err.println("Failed to initialize server: " + e.getMessage());
-        }
-    }
-
-    private void addEndpoints() {
-    	for (InetAddress addr : EndpointManager.getEndpointManager().getNetworkInterfaces()) {
-    		// only binds to IPv4 addresses and localhost
-			if (addr instanceof Inet4Address || addr.isLoopbackAddress()) {
-				InetSocketAddress bindToAddress = new InetSocketAddress(addr, COAP_PORT);
-				addEndpoint(new CoapEndpoint(bindToAddress));
+    	CoapServer server = new CoapServer();
+		server.add(new CoapResource("hello") {
+			@Override
+			public void handleGET(CoapExchange exchange) {
+				exchange.respond(ResponseCode.CONTENT, "Hello Security!");
 			}
-		}
+		});
+		server.add(new CoapResource("time") {
+			@Override
+			public void handleGET(CoapExchange exchange) {
+				
+				Date date = new Date();
+				exchange.respond(ResponseCode.CONTENT, 
+							new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(date));
+			}
+		});
+        server.start();
     }
 
-    public HelloCoAPServer() throws SocketException {
-        add(new HelloWorldResource());
-    }
-
-    class HelloWorldResource extends CoapResource {
-        
-        public HelloWorldResource() {
-            super("hello");
-            getAttributes().setTitle("Hello-World Resource");
-        }
-
-        @Override
-        public void handleGET(CoapExchange exchange) {
-            exchange.respond("Hello CoAP!");
-        }
-    }
 }
