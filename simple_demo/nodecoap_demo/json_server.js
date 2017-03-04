08a14a4ecbb57ba9cdbbceaa426e2f0d3c6420ae
xukai <xukai19871105@126.com>
2017-03-04 13:33:40 +0800

    修改json server示例

--- simple_demo/nodecoap_demo/json_server.js
+++ simple_demo/nodecoap_demo/json_server.js
@@ -4,13 +4,13 @@ const server = coap.createServer()
 server.on('request', function(req, res) {
   console.log('method:' + req.method + ' url:' + req.url)
   if (req.method == 'PUT' && req.url.split('/')[1] == 'multi-format') {
-    if (req.headers['Accept'] != 'application/json') {
-      res.code = '2.04'
-      res.end('payload: ' + req.payload.toString())
-    } else {
+    if (req.headers['Accept'] == 'application/json') {
       res.code = '2.04'
       res.setOption('Content-Format', 'application/json')
       res.end(JSON.stringify({payload: req.payload.toString()}))
+    } else {
+      res.code = '2.04'
+      res.end('payload: ' + req.payload.toString())
     }
   }
 })
